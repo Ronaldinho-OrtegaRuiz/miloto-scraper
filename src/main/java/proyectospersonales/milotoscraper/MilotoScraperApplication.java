c70c8e091436ec711dac211781ae7c5dfd965150
Ronaldinho-OrtegaRuiz <rortegar01@libertadores.edu.co>
2025-05-12 17:11:47 -0500

    first commit

---
+++ src/main/java/proyectospersonales/milotoscraper/MilotoScraperApplication.java
@@ -0,0 +1,13 @@
+package proyectospersonales.milotoscraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilotoScraperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MilotoScraperApplication.class, args);
+    }
+
+}
